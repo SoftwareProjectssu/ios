531c1f302628cbbfedcad17dbf4dccd316352fd2
taebin2 <109895271+taebin2@users.noreply.github.com>
2025-05-07 22:04:02 +0900

    Initial Commit

---
+++ hAIr/hAIrApp.swift
@@ -0,0 +1,17 @@
+//
+//  hAIrApp.swift
+//  hAIr
+//
+//  Created by 한태빈 on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct hAIrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
